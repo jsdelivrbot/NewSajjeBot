5a4e730960c695d4f813e48cddb641cc904e50ed
Indimagedara <nimeshnishara@gmail.com>
2015-10-24 04:37:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
-// Put your own Twitter App keys here. See README.md for more detail.
+
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
+  consumer_key:         'IHUnGHJuIowA9XzK3Q7rujACn',
+  consumer_secret:      'JCart1ETw9dbkILLDxupBfS4ifsKIuBia5m151IZiEv4ZwcV5z',
+  access_token:         '3972856272-O0ntUx3O4mknXy9qjucZGE5xJlaGmnSSqS7zR0D',
+  access_token_secret:  '4CTEKIWPpLruG6A1kchTLkvPjYqT4qTCAnDeQ2w1DzRH7'
 }
