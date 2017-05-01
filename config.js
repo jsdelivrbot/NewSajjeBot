a64dc0d348f350a160e0dd6d4c1774bbab6c3114
Indimagedara <nimeshnishara@gmail.com>
2017-05-01 19:46:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-  consumer_key:         'IHUnGHJuIowA9XzK3Q7rujACn',
-  consumer_secret:      'JCart1ETw9dbkILLDxupBfS4ifsKIuBia5m151IZiEv4ZwcV5z',
-  access_token:         '3972856272-O0ntUx3O4mknXy9qjucZGE5xJlaGmnSSqS7zR0D',
-  access_token_secret:  '4CTEKIWPpLruG6A1kchTLkvPjYqT4qTCAnDeQ2w1DzRH7'
+  consumer_key:         'xxxxxxxxxxx',
+  consumer_secret:      'xxxxxxxxxxx',
+  access_token:         'xxxxxxxxxxx',
+  access_token_secret:  'xxxxxxxxxxxx'
 }
